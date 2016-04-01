eaaf4bf1a173c07101f9f637a85aa5601edf2a5f
Valantis Kamayiannis <kamayiannis@yahoo.com>
2016-04-01 20:32:43 +0300

    added projects model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160401162710) do
+ActiveRecord::Schema.define(version: 20160401173135) do
+
+  create_table "projects", force: true do |t|
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "tasks", force: true do |t|
     t.string   "title"
